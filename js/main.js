283dba4c4ecb1489893febf124748c87e3da438d
ZaidMra3be <Zaidmaraabeh@Icloud.com>
2025-04-18 19:49:14 +0300

    زيد

--- js/main.js
+++ js/main.js
@@ -13,10 +13,10 @@ if(navToggle){
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if (navClose) {
+if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
-    } )
+    })
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
@@ -32,12 +32,40 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader(){
     const header = document.getElementById('header')
-    
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
     if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== QUESTIONS ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.questions__item')
+
+accordionItems.forEach((item) =>{
+    const accordionHeader = item.querySelector('.questions__header')
+
+    accordionHeader.addEventListener('click', () =>{
+        const openItem = document.querySelector('.accordion-open')
+
+        toggleItem(item)
+
+        if(openItem && openItem!== item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) =>{
+    const accordionContent = item.querySelector('.questions__content')
+
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }else{
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+
+}
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
